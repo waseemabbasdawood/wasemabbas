4d4d3d6f311df6b372b1ed66ad52de1fa2db7c87
waseemabbasdawood <waseemabbas239@gmail.com>
2022-09-26 15:34:22 +0500

    new

---
+++ app.js
@@ -0,0 +1,24 @@
+// let student = {
+//     fName: "waseem",
+//     lName: "abbas",
+//     class: "12th",
+//     rollNom: 22,
+//     age: 23,
+//     add: "gilgit"
+// }
+
+// console.log(student.fName)
+// console.log(student.lName)
+
+// console.log(student['fName']+" " + student['lName'])
+
+
+let student1 = ("wei, 12, 3, 20")
+let student2 = ("wei, 12, 3, 20")
+console.log(student1)
+
+
+
+let am = ["student",14,true]
+
+document.write(am)
